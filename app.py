50cde4bc5016c2596abe35ce5c2d808675bf5ec5
anxgzmcrnld <howard115@gmail.com>
2024-12-12 09:53:57 +0000

    add cache to map

--- app.py
+++ app.py
@@ -10,21 +10,48 @@ geolocator = Nominatim(user_agent="my_unique_app_name")  # Change to a unique na
 # Add a text input for user location
 user_location = st.text_input("Enter a location:", "Kaohsiung")  # Default to Kaohsiung
 
-# Get latitude and longitude from user input
-if user_location:
-    location = geolocator.geocode(user_location)
-    if location:
-        latitude, longitude = location.latitude, location.longitude
+@st.cache_data
+def create_map(latitude, longitude, location_name):
+    """Create a folium map with a marker for the given coordinates"""
+    m = folium.Map(location=[latitude, longitude], zoom_start=16)
+    folium.Marker(
+        [latitude, longitude], 
+        popup=location_name, 
+        tooltip=location_name
+    ).add_to(m)
+    return m
+
+@st.cache_data
+def get_location_coordinates(location_name):
+    """Get coordinates for a given location name using geocoding"""
+    try:
+        location = geolocator.geocode(location_name)
+        if location:
+            return location.latitude, location.longitude
+        return None
+    except:
+        return None
+
+# Only update the map if the location has changed
+if user_location != st.session_state.previous_location:
+    st.session_state.previous_location = user_location
+    
+    if user_location:
+        coordinates = get_location_coordinates(user_location)
+        if coordinates:
+            st.session_state.latitude, st.session_state.longitude = coordinates
+        else:
+            st.error("Location not found. Please try another one.")
+            st.session_state.latitude = 39.949610
+            st.session_state.longitude = -75.150282
     else:
-        st.error("Location not found. Please try another one.")
-else:
-    latitude, longitude = 39.949610, -75.150282  # Default to Liberty Bell coordinates
+        st.session_state.latitude = 39.949610
+        st.session_state.longitude = -75.150282
 
-# Update map with user location
-m = folium.Map(location=[latitude, longitude], zoom_start=16)
-folium.Marker(
-    [latitude, longitude], popup=user_location, tooltip=user_location
-).add_to(m)
-
-# call to render Folium map in Streamlit
+# Create and display map using cached function
+m = create_map(
+    st.session_state.latitude, 
+    st.session_state.longitude, 
+    user_location
+)
 st_data = st_folium(m, width=725)
